5dc8271cfbf4490311acb11a253caf425f5376d4
Jakub Nenczak <jnenczak@e3r8p3.42roma.it>
2024-01-10 15:56:53 +0100

    norminette done up to FT_SPLIT

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: jnenczak <jnenczak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 23:26:31 by jakubnencza       #+#    #+#             */
-/*   Updated: 2024/01/10 14:44:28 by jnenczak         ###   ########.fr       */
+/*   Updated: 2024/01/10 15:53:32 by jnenczak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,26 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	char	*ret;
 	size_t	i;
 
-	ret = (char *)malloc(sizeof(char) * len);
+	ret = (char *)malloc(sizeof(char) * (len + 1));
 	i = 0;
-	if (ret == NULL)
+	if (s == NULL || ret == NULL)
 		return (NULL);
 	while (s[start] && i < len)
 	{
 		ret[i++] = s[start];
 		start++;
 	}
+	ret[i] = '\0';
 	return (ret);
 }
 // What about null termination?
+
+#include <stdio.h>
+
+int	main(void)
+{
+	const char *str = "Hello world!";
+	printf("orig: %s\n", str);
+	printf("substr: %s\n", ft_substr(str, 2, 6));
+	return (0);
+}
