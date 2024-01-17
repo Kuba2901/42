07c638cba401b354f2c3698db86c7649e305f777
Jakub Nenczak <kubanenczak@gmail.com>
2024-01-17 17:39:02 +0100

    starting tests

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jakubnenczak <jakubnenczak@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 19:40:37 by jakubnencza       #+#    #+#             */
-/*   Updated: 2024/01/17 17:35:14 by jakubnencza      ###   ########.fr       */
+/*   Updated: 2024/01/17 17:39:00 by jakubnencza      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,11 @@
 
 char	*get_next_line(int fd)
 {
-	char		*ret;
-	char		c;
-	int			index;
-	// static int	last;
-	int			endl_reached;
+	char	*ret;
+	char	c;
+	int		index;
+	int		endl_reached;
 
-	// last = 0;
 	endl_reached = 0;
 	ret = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	index = 0;
@@ -38,7 +36,6 @@ char	*get_next_line(int fd)
 		while (read(fd, &c, 1))
 			if (c == '\n')
 				break ;
-		// last = read(fd, &c, 1);
 	}
 	else
 		ret[index++] = '\n';
@@ -46,14 +43,14 @@ char	*get_next_line(int fd)
 	return (ret);
 }
 
-int	main(void)
-{
-	int fd = open("test", O_RDONLY);
-	char *ret = get_next_line(fd);
-	char *ret2 = get_next_line(fd);
-	printf("ret1: %s\n", ret);
-	printf("ret2: %s\n", ret2);
-	free(ret);
-	free(ret2);
-	return (0);
-}
+// int	main(void)
+// {
+// 	int fd = open("test", O_RDONLY);
+// 	char *ret = get_next_line(fd);
+// 	char *ret2 = get_next_line(fd);
+// 	printf("ret1: %s\n", ret);
+// 	printf("ret2: %s\n", ret2);
+// 	free(ret);
+// 	free(ret2);
+// 	return (0);
+// }
