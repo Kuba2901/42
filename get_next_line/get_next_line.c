ab9310302fac2fc23172c82f9860ea5b68ca4888
Jakub Nenczak <kubanenczak@gmail.com>
2024-01-29 16:12:23 +0100

    leaks

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -15,6 +15,29 @@
 #include <stdio.h>
 #include <fcntl.h>
 
+char	*ft_get_line(char *buff)
+{
+	int		i;
+	char	*ret;
+
+	i = -1;
+	while (buff[++i])
+	{
+		if (buff[i] == '\n')
+		{
+			i++;
+			break ;
+		}
+	}
+	ret = (char *)malloc(sizeof(char) * (i + 1));
+	if (!ret)
+		return (NULL);
+	ret[i] = '\0';
+	while (--i >= 0)
+		ret[i] = buff[i];
+	return (ret);
+}
+
 char	*ft_reassign(char *buff, char *new)
 {
 	char	*ret;
@@ -22,95 +45,73 @@ char	*ft_reassign(char *buff, char *new)
 	ret = ft_strjoin(buff, new);
 	free(buff);
 	free(new);
-	if (*ret == '\0')
-	{
-		free(ret);
-		return (NULL);
-	}
 	return (ret);
 }
 
-char	*ft_read_buffer(int fd)
+char	*ft_read_line(int fd)
 {
-	char	*ret;
-	char	*temp;
-	size_t	rbytes;
+	char			*line;
+	char			*temp;
+	unsigned int	rbytes;
 
-	ret = (char *)malloc(sizeof(char) * BUFFER_SIZE);
-	if (!ret)
+	line = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!line)
 		return (NULL);
-	rbytes = read(fd, ret, BUFFER_SIZE);
-	ret[rbytes] = '\0';
-	while (rbytes > 0)
+	rbytes = read(fd, line, BUFFER_SIZE);
+	if (rbytes <= 0)
 	{
-		temp = (char *)malloc(sizeof(char) * BUFFER_SIZE);
+		free(line);
+		return (NULL);
+	}
+	line[rbytes] = '\0';
+	while (!ft_strchr(line, '\n') && rbytes > 0)
+	{
+		temp = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+		if (!temp)
+			break ;
 		rbytes = read(fd, temp, BUFFER_SIZE);
 		if (!rbytes)
 		{
 			free(temp);
-			break ;
+			break ; 
 		}
-		ret = ft_reassign(ret, temp);
+		temp[rbytes] = '\0';
+		line = ft_reassign(line, temp);
 	}
-	return (ret);
-}
-
-char	*ft_get_line(const char *str)
-{
-	unsigned int	i;
-	char			*ret;
-
-	i = -1;
-	if (str == NULL || *str == '\0')
-		return (NULL);
-	while (str[++i])
-	{
-		if (str[i] == '\n')
-		{
-			i++;
-			break ;
-		}
-	}
-	ret = ft_substr(str, 0, i);
-	return (ret);
-}
-
-char	*ft_reassign_mark(const char *mark, char *line)
-{
-	char	*ret;
-
-	ret = ft_strdup(mark + ft_strlen(line));
-	return (ret);
+	return (line);
 }
 
 char	*get_next_line(int fd)
 {
 	static char	*mark = NULL;
 	char		*ret;
-	char		*temp;
+	char		*buff;
 
-	if (fd < 0 || BUFFER_SIZE <= 0)
-		return (NULL);
-	if (!mark)
-		temp = ft_read_buffer(fd);
-	ret = ft_get_line(mark);
-	if (ret != NULL)
-		mark = ft_reassign_mark(mark, ret);
+
+	buff = ft_read_line(fd);
+	if (!buff)
+	{
+		ret = ft_strdup(mark);
+		mark = NULL;
+		return (ret);
+	}
+	ret = ft_get_line(buff);
+	mark = ft_strdup(buff + ft_strlen(ret));
+	free(buff);
 	return (ret);
 }
 
-#include <string.h>
+// #include <string.h>
 
-int	main(void)
-{
-	int fd = open("files/41_with_nl_copy", O_RDONLY);
-	char *ret;
-
-	while ((ret = get_next_line(fd)) != NULL)
-	{
-		printf("line: %s\n", ret);
-		free(ret);
-	}
-	close(fd);
-	return (0);
-}
+// int	main(void)
+// {
+// 	int fd = open("files/41_with_nl_copy", O_RDONLY);
+// 	char *ret;
+// 	while ((ret = get_next_line(fd)) != NULL)
+// 	{
+// 		printf("%s", ret);
+// 		free(ret);
+// 	}
+// 	close(fd);
+// 	return (0);
+// }
