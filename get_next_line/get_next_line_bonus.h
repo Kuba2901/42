be6899a2748edac04d11335955f2c39dfc32fd8f
Jakub Nenczak <kubanenczak@gmail.com>
2024-02-01 14:14:40 +0100

    cleaning

---
+++ get_next_line/get_next_line_bonus.h
@@ -0,0 +1,21 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <fcntl.h>
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+char	*ft_strchr(const char *s, int c);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+char	*ft_strjoin(char const *s1, char const *s2);
+int		ft_strlen(const char *str);
+char	*ft_strdup(const char *s1);
+char	*ft_join_reassign(char *buffer, char *to_join);
+char	*ft_get_line_from_buff(char *buff);
+char	*ft_read_until_nl(int fd);
+char	*get_next_line(int fd);
+
+#endif
