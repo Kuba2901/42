54cb8a79eb4f91a4aacd8ac8a3b35d725511b712
Jakub Nenczak <kubanenczak@gmail.com>
2024-02-24 11:46:38 +0000

    working on push_swap

--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -1,20 +1,31 @@
 #include "push_swap.h"
 
-// int	*parse_args(int argc, char **argv)
-// {
-// 	int	i;
-// 	int	*ret;
-
-// 	i = 1;
-// 	ret = (int *)malloc(sizeof(int) * (argc - 1));
-// 	while (i < argc)
-// 	{
-
-// 	}
-// }
-
-int	main(void)
+int	*parse_args(int argc, char **argv)
 {
-	ft_printf("out: %d\n", ft_atoi("4231"));
+	int	i;
+	int	*ret;
+
+	i = 0;
+	ret = (int *)malloc(sizeof(int) * (argc - 1));
+	if (!ret)
+		return (NULL);
+	while (i < argc - 1)
+	{
+		ret[i] = ft_atoi(argv[i + 1]);
+		i++;
+	}
+	return (ret);
+}
+
+int	main(int ac, char **av)
+{
+	if (ac != 1)
+	{
+		int	*nums = parse_args(ac, av);
+		int i = -1;
+		while (++i < ac - 1)
+			ft_printf("num {%d}\n",nums[i]);
+
+	}
 	return (0);
 }
