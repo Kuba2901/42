1d209093d0fd8ef154cbf0f3904d85c965adb0eb
Jakub Nenczak <kubanenczak@gmail.com>
2024-03-21 17:29:18 +0100

    compiled custom printf

--- so_long/libft/inc_libft/libft.h
+++ so_long/libft/inc_libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: jnenczak <jnenczak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 17:10:34 by jakubnencza       #+#    #+#             */
-/*   Updated: 2024/02/05 14:27:11 by jnenczak         ###   ########.fr       */
+/*   Updated: 2024/03/21 17:28:17 by jnenczak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,9 @@
 # include <stddef.h>
 # include <stdlib.h>
 # include <unistd.h>
+# include <ft_printf.h>
+# define FT_STDOUT 1
+
 
 typedef struct s_list
 {
