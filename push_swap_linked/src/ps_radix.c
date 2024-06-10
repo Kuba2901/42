3a5d995619b80c1183ebc7c2532850ae5314fee0
Jakub Nenczak <kubanenczak@gmail.com>
2024-06-10 17:33:03 +0200

    Working with negative numbers

--- push_swap_linked/src/ps_radix.c
+++ push_swap_linked/src/ps_radix.c
@@ -6,11 +6,18 @@
 /*   By: jnenczak <jnenczak@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/10 16:29:53 by jnenczak          #+#    #+#             */
-/*   Updated: 2024/06/10 16:37:31 by jnenczak         ###   ########.fr       */
+/*   Updated: 2024/06/10 17:32:48 by jnenczak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <push_swap.h>
+
+static int	ps_abs(int num)
+{
+	if (num < 0)
+		return -num;
+	return num;
+}
 
 void	ps_radix_sort(t_list **stack, t_list **other_stack)
 {
@@ -20,11 +27,11 @@ void	ps_radix_sort(t_list **stack, t_list **other_stack)
 	int		size;
 
 	temp = *stack;
-	max_num = ps_elem_value(temp);
+	max_num = ps_abs(ps_elem_value(temp));
 	while (temp)
 	{
-		if (ps_elem_value(temp) > max_num)
-			max_num = ps_elem_value(temp);
+		if (abs(ps_elem_value(temp)) > max_num)
+			max_num = ps_abs(ps_elem_value(temp));
 		temp = temp->next;
 	}
 
@@ -32,15 +39,31 @@ void	ps_radix_sort(t_list **stack, t_list **other_stack)
     while ((max_num >> max_bits) != 0)
         max_bits++;
 
+    // Since integers can be negative, we need an additional bit for the sign
+    max_bits++;
+
     for (int i = 0; i < max_bits; i++) {
         size = ft_lstsize(*stack);
         while (size--) {
-			if (((ps_elem_value(*stack) >> i) & 1) == 1)
-				ps_rx(stack);
-            else
-				ps_px(stack, other_stack);
+            int value = ps_elem_value(*stack);
+            // If we are checking the sign bit
+            if (i == max_bits - 1) {
+                if (value < 0)
+                    ps_rx(stack);
+                else
+                    ps_px(stack, other_stack);
+            } else {
+                if (((value >> i) & 1) == 1)
+                    ps_rx(stack);
+                else
+                    ps_px(stack, other_stack);
+            }
         }
-		while (ft_lstsize(*other_stack) > 0)
-			ps_px(other_stack, stack);
+        while (ft_lstsize(*other_stack) > 0)
+            ps_px(other_stack, stack);
     }
+
+	// Check if negative numbers did not break the order and rotate
+	while (!is_sorted(stack))
+		ps_rrx(stack);
 }
